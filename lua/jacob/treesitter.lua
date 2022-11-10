e3a817f1a8f13e470c9ef17d5e70e021ea4a55aa
Jacob Atzen <jatzen@gmail.com>
2022-11-10 08:02:35 +0100

    Treesitter tweaks

--- lua/jacob/treesitter.lua
+++ lua/jacob/treesitter.lua
@@ -1,7 +1,8 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"elixir", "heex", "eex"}, -- only install parsers for elixir and heex
+  ensure_installed = {"lua", "ruby", "elixir", "heex", "eex"}, -- only install parsers for elixir and heex
   -- ensure_installed = "all", -- install parsers for all supported languages
   sync_install = false,
+  playground = { enable = true },
   ignore_install = { },
   highlight = {
     enable = true,
