c0f54c6367c28ef2fef26f3af66d78717292aab7
Jacob Atzen <jatzen@gmail.com>
2023-09-02 18:00:23 +0200

    Add treesitter textobjects

--- lua/jacob/plugins/init.lua
+++ lua/jacob/plugins/init.lua
@@ -3,4 +3,5 @@ require("jacob.plugins.lazy").setup({
 	require("jacob.plugins.lualine"),
 	require("jacob.plugins.telescope"),
 	require("jacob.plugins.treesitter"),
+	require("jacob.plugins.treesitter-textobjects"),
 })
