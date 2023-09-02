18db21db453e4a12eea0dee95b324faee97ee1dd
Jacob Atzen <jatzen@gmail.com>
2023-09-02 18:07:18 +0200

    Add indent-blankline

---
+++ lua/jacob/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  opts = {
+    -- char = '┊',
+    char = '⋅',
+    show_trailing_blankline_indent = false,
+    show_current_context = false,
+    show_current_context_start = false,
+  }
+}
--- lua/jacob/plugins/init.lua
+++ lua/jacob/plugins/init.lua
@@ -4,4 +4,5 @@ require("jacob.plugins.lazy").setup({
 	require("jacob.plugins.telescope"),
 	require("jacob.plugins.treesitter"),
 	require("jacob.plugins.treesitter-textobjects"),
+	require("jacob.plugins.indent-blankline"),
 })
