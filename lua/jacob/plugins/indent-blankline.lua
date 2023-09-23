00649a1ab12c16a6c131213b714a53128523a59e
Jacob Atzen <jatzen@gmail.com>
2023-09-23 23:30:58 +0200

    Tweak indent-blankline

--- lua/jacob/plugins/indent-blankline.lua
+++ lua/jacob/plugins/indent-blankline.lua
@@ -2,9 +2,12 @@ return {
   'lukas-reineke/indent-blankline.nvim',
   opts = {
     -- char = '┊',
-    char = '⋅',
+    -- char = '⋅',
     show_trailing_blankline_indent = false,
     show_current_context = false,
     show_current_context_start = false,
+    use_treesitter_scope = true,
+    -- char_list = {'|', '¦', '┆', '┊'},
+    -- char_list_blankline = {'|', '¦', '┆', '┊'},
   }
 }
