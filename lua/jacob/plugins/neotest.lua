7fe020c8036c555753da811b30ac33ce4e1892bd
Jacob Atzen <jatzen@gmail.com>
2024-05-08 20:43:15 +0200

    Fix neotest

--- lua/jacob/plugins/neotest.lua
+++ lua/jacob/plugins/neotest.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
     "jfpedroza/neotest-elixir",
     "olimorris/neotest-rspec",
   },
