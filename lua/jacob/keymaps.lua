e646da337be780bf951b69cfde6c712d647ec6ea
Jacob Atzen <jatzen@gmail.com>
2024-06-30 14:54:20 +0200

    Add some keymaps for folding and diagnostics

--- lua/jacob/keymaps.lua
+++ lua/jacob/keymaps.lua
@@ -42,6 +42,18 @@ keymap.set("n", "<leader>gv", ":edit $MYVIMRC<CR>")
 -- Edit files in same directory as current buffer
 keymap.set("n", "<leader>e", ":edit <C-R>=expand('%:p:h') . \"/\" <CR>")
 
+-- Folding keymaps
+keymap.set("n", "<leader>ft", ":set foldmethod=expr foldexpr=nvim_treesitter#foldexpr()<CR>")
+keymap.set("n", "<leader>fen", ":set fen<CR>")
+keymap.set("n", "<leader>fno", ":set nofen<CR>")
+
+-- Diagnostic keymaps
+vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
