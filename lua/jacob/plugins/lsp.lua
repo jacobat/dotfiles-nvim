a99b11898df8f850dce8d97da13730861c5d41b7
Jacob Atzen <jatzen@gmail.com>
2024-09-21 11:27:13 +0200

    Find references

--- lua/jacob/plugins/lsp.lua
+++ lua/jacob/plugins/lsp.lua
@@ -3,6 +3,16 @@ return {
     config = function()
       require'lspconfig'.solargraph.setup{}
       require'lspconfig'.elmls.setup{}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client.supports_method('referencesProvider') then
+            vim.keymap.set("n", "<leader>rr", "<cmd>lua vim.lsp.buf.references(nil, { loclist = true })<cr>", { noremap = true, silent = true })
+          end
+        end,
+      })
+
     end
   }
 }
