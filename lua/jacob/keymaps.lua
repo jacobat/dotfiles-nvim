9b643cb58672e0e974e4992e3914d1d33bdf1dd5
Jacob Atzen <jatzen@gmail.com>
2024-10-23 13:12:48 +0200

    Keymap to copy buffer path to paste buffer

--- lua/jacob/keymaps.lua
+++ lua/jacob/keymaps.lua
@@ -17,6 +17,9 @@ keymap.set("n", "<leader>n", ":nohl<CR>")
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
 
+-- Copy buffer path to paste buffer
+keymap.set("n", "<leader>cp", ":let @+ = expand('%:p')<CR>")
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>") -- increment
 keymap.set("n", "<leader>-", "<C-x>") -- decrement
