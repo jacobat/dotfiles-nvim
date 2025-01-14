f3b9563edf7a62d9b045999c8a7c329bdf02d80b
Jacob Atzen <jatzen@gmail.com>
2025-01-14 13:17:44 +0100

    Fix Ruby indentation by disabling treesitter indent
    Tree indent fails on
    
    case ...
    in ...
    end

--- lua/jacob/plugins/treesitter.lua
+++ lua/jacob/plugins/treesitter.lua
@@ -23,9 +23,9 @@ return {
 
       ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
       -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-      indent = {
-        enable = true
-      },
+      -- indent = {
+      --   enable = false
+      -- },
 
       highlight = {
         enable = true,
@@ -48,7 +48,7 @@ return {
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
       },
 
       textobjects = {
