2d396ee2d646f9c642d80c41cf2626794d6c2b4a
Jacob Atzen <jatzen@gmail.com>
2025-01-23 22:26:22 +0100

    Enable mini.ai and mini.surround

--- lua/jacob/plugins/mini.lua
+++ lua/jacob/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
-    -- require('mini.ai').setup()
-    -- require('mini.surround').setup()
+    require('mini.ai').setup()
+    require('mini.surround').setup()
   end
 }
