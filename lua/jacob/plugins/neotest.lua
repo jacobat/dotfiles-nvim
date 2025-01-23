cbb3c0bd06ff30b419d04d4352fcff059efd5f8b
Jacob Atzen <jatzen@gmail.com>
2025-01-23 16:52:55 +0100

    Remove FixCursorHold plugin
    Let's see if this fixes the sluggishness and excessive CPU usage

--- lua/jacob/plugins/neotest.lua
+++ lua/jacob/plugins/neotest.lua
@@ -3,7 +3,6 @@ return {
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "jfpedroza/neotest-elixir",
     "olimorris/neotest-rspec",
