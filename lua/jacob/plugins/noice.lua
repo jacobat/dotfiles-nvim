de65e6b5a45e93c7a42bb93c63fb5830885ecf61
Jacob Atzen <jatzen@gmail.com>
2025-01-24 07:56:42 +0100

    Use mini for noice messages

--- lua/jacob/plugins/noice.lua
+++ lua/jacob/plugins/noice.lua
@@ -28,6 +28,12 @@ return {
           ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
       },
+
+      messages = {
+        view = "mini",
+        view_error = "mini",
+        view_warn = "mini",
+      },
       -- you can enable a preset for easier configuration
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
