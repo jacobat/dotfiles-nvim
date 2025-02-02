6e60a6b0e44a104c1e751f6e3c38e8c92d7e4d13
Jacob Atzen <jatzen@gmail.com>
2025-02-02 17:12:03 +0100

    Mucking around

--- init.lua
+++ init.lua
@@ -3,3 +3,27 @@ require("jacob.keymaps")
 require("jacob.plugins")
 
 vim.cmd("colorscheme carbonfox")
+
+function run_tests()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, true, {"test", "text2"})
+  local opts = {
+    relative = 'cursor',
+    border = 'rounded',
+    width = 80,
+    height = 20,
+    col =  0,
+    row = 1,
+    anchor =  'NW',
+    style =  'minimal',
+    title = "[ rspec ]",
+  }
+
+  local win = vim.api.nvim_open_win(buf, true, opts)
+end
+
+-- run_tests()
+
+vim.keymap.set('n', "<leader>cpw", function() run_tests() end, {})
+vim.keymap.set('v', "<leader>x", "<cmd>'<,'>lua<CR>", { desc = "Execute visual selection" })
+vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
