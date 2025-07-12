3fb742612dc3de8935f8605308c57861da911a64
Jacob Atzen <jatzen@gmail.com>
2025-07-12 15:20:22 +0200

    WSL copy paste

--- lua/jacob/settings.lua
+++ lua/jacob/settings.lua
@@ -50,3 +50,24 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 vim.api.nvim_command("au BufWritePost *.ex,*.exs,*.heex lua vim.lsp.buf.format()")
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = { "clip.exe" },
+    ["*"] = { "clip.exe" },
+  },
+  paste = {
+    ["+"] = {
+      "/mnt/c/Windows/System32/WindowsPowerShell/v1.0///powershell.exe",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    ["*"] = {
+      "/mnt/c/Windows/System32/WindowsPowerShell/v1.0///powershell.exe",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+  },
+  cache_enabled = false,
+}
