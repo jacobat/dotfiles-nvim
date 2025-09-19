67d923656b4a49a4bb2d0ae461a9ecba822cbd7b
Jacob Atzen <jatzen@gmail.com>
2025-09-19 09:06:10 +0200

    luals

--- lua/jacob/plugins/lsp.lua
+++ lua/jacob/plugins/lsp.lua
@@ -13,6 +13,32 @@ return {
       })
       require'lspconfig'.elmls.setup{}
 
+      vim.lsp.config['luals'] = {
+        -- Command and arguments to start the server.
+        cmd = { 'lua-language-server' },
+        -- Filetypes to automatically attach to.
+        filetypes = { 'lua' },
+        -- Sets the "workspace" to the directory where any of these files is found.
+        -- Files that share a root directory will reuse the LSP server connection.
+        -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+        root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+        -- Specific settings to send to the server. The schema is server-defined.
+        -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            runtime = {
+              version = 'LuaJIT',
+            }
+          }
+        }
+      }
+
+      vim.lsp.enable('luals')
+
+
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
