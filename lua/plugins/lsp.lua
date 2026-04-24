58882475684e514cc0bb832585dfe6499742a8f0
Jacob Atzen <jatzen@gmail.com>
2026-04-24 13:36:11 +0200

    Fix lsp deprecation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,10 +51,10 @@ end
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.supports_method('referencesProvider') then
+    if client:supports_method('referencesProvider') then
       vim.keymap.set("n", "<leader>rr", "<cmd>lua vim.lsp.buf.references(nil, { loclist = true })<cr>", { noremap = true, silent = true })
     end
-    if client.supports_method('textDocument/codeAction') then
+    if client:supports_method('textDocument/codeAction') then
       vim.keymap.set("n", "<leader>rc", "<cmd>lua vim.lsp.buf.code_action()<cr>", { noremap = true, silent = true })
     end
     vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
