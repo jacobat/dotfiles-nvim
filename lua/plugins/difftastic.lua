9b2807142707e069a4cc0983ab27447d78478d0a
Jacob Atzen <jatzen@gmail.com>
2026-09-24 11:20:50 +0200

    Difftastic added

---
+++ lua/plugins/difftastic.lua
@@ -0,0 +1,16 @@
+return {
+    "clabby/difftastic.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        -- optional: only needed for :DifftPick
+        "folke/snacks.nvim",
+    },
+    config = function()
+        require("difftastic-nvim").setup({
+            download = true, -- Auto-download pre-built binary
+            snacks_picker = {
+                enabled = true,
+            },
+        })
+    end,
+}
